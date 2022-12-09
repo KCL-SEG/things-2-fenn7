774218a69600d3ea21c726c846fd148f278e708f
fenn7 <afenn666@gmail.com>
2022-12-09 15:53:55 +0000

    Update views.py

--- things/views.py
+++ things/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Thing
+from .forms import ThingForm
 
 def home(request):
-    return render(request, 'home.html', {'form': form})
+    if request.method == "POST":
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            return render(request, 'home.html', {'form': form})
+    form = StudentSignUpForm()
+    return render(request, "home.html", {"form": form})
+    
